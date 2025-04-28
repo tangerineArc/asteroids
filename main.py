6865df4a594473f588aac6096771f5f10282f26c
Swagatam <swagatamheroz@outlook.com>
2025-04-29 01:55:30 +0530

    chore: draw player to screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import constants
+import player
 import pygame
 
 
@@ -9,7 +10,12 @@ def main():
 
   pygame.init()
 
+  clock = pygame.time.Clock()
+  dt = 0
+
   screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+  p = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
   while True:
     for event in pygame.event.get():
@@ -17,8 +23,11 @@ def main():
         return
 
     screen.fill("#000000")
+    p.draw(screen)
 
     pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
